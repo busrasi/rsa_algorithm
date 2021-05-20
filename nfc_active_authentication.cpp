192885b4b40b65f4bb24bceb763aefc08aea36f1
Muhammet Küçük <muhammet.kucuk@kobil.com>
2021-05-20 10:27:07 +0300

    wip

---
+++ nfc_active_authentication.cpp
@@ -0,0 +1,15 @@
+#include "nfc_active_authentication.h"
+#include <openssl/rsa.h>
+#include <openssl/pem.h>
+
+constexpr auto const symmetricText = "Forza Buca";
+
+NFCActiveAuthentication::NFCActiveAuthentication()
+{
+
+}
+
+void NFCActiveAuthentication::generateRSAKeyPair()
+{
+
+}
---
+++ nfc_active_authentication.h
@@ -0,0 +1,15 @@
+#ifndef NFCACTIVEAUTHENTICATION_H
+#define NFCACTIVEAUTHENTICATION_H
+
+
+class NFCActiveAuthentication
+{
+
+public:
+    NFCActiveAuthentication();
+
+private:
+
+};
+
+#endif // NFCACTIVEAUTHENTICATION_H
