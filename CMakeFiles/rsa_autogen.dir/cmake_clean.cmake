19ad56feab5f7085fb49398631bf10248ca1d36b
Muhammet Küçük <muhammet.kucuk@kobil.com>
2021-05-20 10:21:58 +0300

    git ignore

--- CMakeFiles/rsa_autogen.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/rsa_autogen"
-  "rsa_autogen/mocs_compilation.cpp"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/rsa_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
